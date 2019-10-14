2545f61087eb65e88c4585027ac5d5c37a9464f4
Vlad Pistun <v.pistun@1plus1.digital>
2019-10-14 21:06:59 +0300

    Fixed

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"net/http"
+	"time"
 )
 
 func main() {
+
+	go DoEvery(10*time.Second)
+
 	http.ListenAndServe(":5000", ChiRouter().InitRouter())
 }
---
+++ task.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"log"
+	"test-task/infrastructures"
+	"test-task/repositories"
+	"test-task/services"
+	"time"
+)
+
+func init() {
+	config := infrastructures.Config{}
+
+	db, err := sql.Open("postgres", config.GetConf().PostgreSQLConn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	postgreSQLHandler = &infrastructures.PostgreSQLHandler{}
+	postgreSQLHandler.Conn = db
+
+	orderRepository = &repositories.OrderRepository{IDbHandler: postgreSQLHandler}
+	userRepository = &repositories.UserRepository{IDbHandler: postgreSQLHandler}
+	productRepository = &repositories.ProductRepository{IDbHandler: postgreSQLHandler}
+	cartRepository = &repositories.CartRepository{IDbHandler: postgreSQLHandler}
+
+	userService = &services.UserService{IUserRepository: userRepository}
+	productService = &services.ProductService{IProductRepository: productRepository, ICartRepository: cartRepository}
+	cartService = &services.CartService{ICartRepository: cartRepository, IProductService: productService, IUserService: userService}
+
+	orderService = &services.OrderService{IOrderRepository: orderRepository, IProductService: productService, IUserService: userService}
+	paymentService = &infrastructures.SolidPayments{
+		IOrderService: orderService,
+		ICartService: cartService,
+		IUserService: userService,
+	}
+}
+
+func DoEvery(d time.Duration) {
+	for _ = range time.Tick(d) {
+		Task()
+	}
+}
+
+// Spider scans website's market.
+func Task() {
+	list := orderService.GetUnProcessedOrders("processing")
+
+	log.Print(list)
+
+	for _, elem := range list {
+		_, err := paymentService.StatusCheck(elem.Id)
+
+		if err != nil {
+			log.Print(err)
+		}
+
+		log.Print(fmt.Sprintf("Order %d processed", elem.Id))
+	}
+}
