2c0931ef0d1e87a5252e57020c5059b373eb77ff
Vlad Pistun <v.pistun@1plus1.digital>
2019-10-15 10:20:11 +0300

    Added auto increment start point

--- setup.sql
+++ setup.sql
@@ -8,5 +8,6 @@ INSERT INTO test.product (id, name, price) VALUES (1, 'Corsair GS600 600 Watt PS
 CREATE TABLE test.user (id SERIAL PRIMARY KEY, name VARCHAR, email VARCHAR, cardToken VARCHAR default NULL);
 
 CREATE TABLE test.order (id SERIAL PRIMARY KEY, name VARCHAR, productId int, price float, status VARCHAR, userId INTEGER, formToken VARCHAR default NULL, createdAt TIMESTAMP , updatedAt TIMESTAMP);
+ALTER SEQUENCE test.order_id_seq RESTART WITH 200;
 
-INSERT INTO test.user (id, name, email) VALUES (1, 'Vlad', 'v.pistun@gmail.com');
+INSERT INTO test.user (name, email) VALUES ('Vlad', 'v.pistun@gmail.com');
